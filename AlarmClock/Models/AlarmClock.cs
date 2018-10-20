ae76a86410df0ff14a699c611d220a0288ae25d5
Макс Недашковский <max0350@gmail.com>
2018-10-21 01:27:34 +0300

    Added User and AlarmClock models

---
+++ AlarmClock/Models/AlarmClock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock.Models
+{
+    class AlarmClock
+    {
+        #region Fields
+        private Guid _guid;
+        private DateTime _lastTriggerDate;
+        private DateTime _nextTriggerDate;
+        #endregion
+
+        #region Fields
+        public Guid Guid
+        {
+            get { return _guid; }
+            set { _guid = value; }
+        }
+
+        public DateTime LastTriggerDate
+        {
+            get { return _lastTriggerDate; }
+            set { _lastTriggerDate = value; }
+        }
+
+        public DateTime NextTriggerDate
+        {
+            get { return _nextTriggerDate; }
+            set { _nextTriggerDate = value; }
+        }
+        #endregion
+
+        #region Constructor
+        public AlarmClock(Guid guid, DateTime lastTriggerDate, DateTime nextTriggerDate)
+        {
+            _guid = Guid.NewGuid();
+            _lastTriggerDate = lastTriggerDate;
+            _nextTriggerDate = nextTriggerDate;
+        }
+
+        private AlarmClock()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"The alarm clock will trigger at {_nextTriggerDate}";
+        }
+        #endregion
+    }
+}
