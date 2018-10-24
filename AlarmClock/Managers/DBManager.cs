df4f7b87df44b191909eb7bb87c6dce3fbd81ba4
Макс Недашковский <max0350@gmail.com>
2018-10-24 22:21:10 +0300

    added managers

---
+++ AlarmClock/Managers/DBManager.cs
@@ -0,0 +1,29 @@
+﻿using AlarmClock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock.Managers
+{
+    public class DBManager
+    {
+        private static readonly List<User> Users = new List<User>();
+
+        public static bool UserExists(string login)
+        {
+            return Users.Any(u => u.Login == login);
+        }
+
+        public static User GetUserByLogin(string login)
+        {
+            return Users.FirstOrDefault(u => u.Login == login);
+        }
+
+        public static void AddUser(User user)
+        {
+            Users.Add(user);
+        }
+    }
+}
