3032eccb54ad9b32f34ff390d40deb99aa7d0dc5
Макс Недашковский <max0350@gmail.com>
2018-11-06 23:57:33 +0300

    deleted unused usings

--- AlarmClock/ViewModels/AlarmClocks/AlarmClock/AlarmClockViewModel.cs
+++ AlarmClock/ViewModels/AlarmClocks/AlarmClock/AlarmClockViewModel.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
 using System.Windows.Input;
 using AlarmClock.Managers;
 using AlarmClock.Models;
