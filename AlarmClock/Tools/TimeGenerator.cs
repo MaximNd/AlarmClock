4a261b5345ec21268a3b5a3365c9e0db61e1e5ef
Макс Недашковский <max0350@gmail.com>
2018-11-07 21:22:05 +0300

    added TimeGenerator into tools

---
+++ AlarmClock/Tools/TimeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock.Tools
+{
+    internal static class TimeGenerator
+    {
+        public static string GetFormattedTime(int time)
+        {
+            return time < 10 ? $"0{time}" : $"{time}"; ;
+        }
+
+        public static List<string> GenerateHours()
+        {
+            List<string> hours = new List<string>();
+            for (int i = 0; i < 24; ++i)
+            {
+                hours.Add(GetFormattedTime(i));
+            }
+
+            return hours;
+        }
+
+        public static List<string> GenerateMinutes()
+        {
+            List<string> minutes = new List<string>();
+            for (int i = 0; i < 60; ++i)
+            {
+                minutes.Add(GetFormattedTime(i));
+            }
+
+            return minutes;
+        }
+
+    }
+}
