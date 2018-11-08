f874b8b984b74b2dad1630abcc20945d63003eaa
Макс Недашковский <max0350@gmail.com>
2018-11-09 00:28:53 +0300

    fixed wrong naming

--- AlarmClock/Tools/FileFolderHelper.cs
+++ AlarmClock/Tools/FileFolderHelper.cs
@@ -8,15 +8,15 @@ namespace AlarmClock.Tools
         private static readonly string AppDataPath =
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         internal static readonly string ClientFolderPath =
-            Path.Combine(AppDataPath, "WalletSimulator");
+            Path.Combine(AppDataPath, "AlarmClock");
         internal static readonly string LogFolderPath =
             Path.Combine(ClientFolderPath, "Log");
         internal static readonly string LogFilepath = Path.Combine(LogFolderPath,
             "App_" + DateTime.Now.ToString("YYYY_MM_DD") + ".txt");
         internal static readonly string StorageFilePath =
-            Path.Combine(ClientFolderPath, "Storage.walsim");
+            Path.Combine(ClientFolderPath, "Storage.alarmclock");
         internal static readonly string LastUserFilePath =
-            Path.Combine(ClientFolderPath, "LastUser.walsim");
+            Path.Combine(ClientFolderPath, "LastUser.alarmclock");
         internal static void CheckAndCreateFile(string filePath)
         {
             try
